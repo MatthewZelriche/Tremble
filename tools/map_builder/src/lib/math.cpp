1d80a7f7d69e76d07ecfbf1faa3781ba11b818b8
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-02-24 14:05:49 -0600

    Map parser math change.
    Change plane equation to one found in reference material, for safety.

--- tools/map_builder/src/lib/math.cpp
+++ tools/map_builder/src/lib/math.cpp
@@ -13,12 +13,14 @@ float TR::Angle(const Vec3 &v1, const Vec3 &v2) {
 TR::Vec3 TR::Normalize(const Vec3 &vector) { return glm::normalize(vector); }
 
 float TR::SignedDistToPlane(const PlaneEq &plane, const Vec3 &point) {
-   return glm::dot(plane.normal, point) + plane.dist;
+   // Have to negate the result for some reason. Something to do with how
+   // we define the plane equation?
+   return -(glm::dot(plane.normal, point) + plane.dist);
 }
 
 TR::PlaneEq TR::CreatePlane(const Vec3 &p1, const Vec3 &p2, const Vec3 &p3) {
    TR::PlaneEq equation;
-   equation.normal = glm::normalize(glm::cross(p3 - p2, p2 - p1));
+   equation.normal = glm::normalize(glm::cross(p3 - p2, p1 - p2));
    equation.point = p1;
    equation.dist = -glm::dot(equation.normal, p1);
    return equation;
